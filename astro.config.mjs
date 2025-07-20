8357a005ab0474e45b04cf9b2fd027883d305a42
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-07-20 16:30:17 -0600

    Deploy Astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind(), react()],
-
+  base: '/Portfolio/',
 });
