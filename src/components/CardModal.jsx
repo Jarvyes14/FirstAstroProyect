3029712bffba4cf89f036e7ed7a8c02b3affab82
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-07-25 07:45:56 -0600

    Fix(CardModal): Change the render.
    Fix the font sizes for every type of display (xs, sm, md, xl, 2xl).
    Add the zoom=false propertie to disable the zoom on a selected card.
    Change the empty value for non-image card from '' to null.

--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 import ReactDOM from "react-dom";
 
-export default function CardModal({ img, title, subtitle, text }) {
+export default function CardModal({ img, title, subtitle, text, zoom}) {
     const [isOpen, setIsOpen] = useState(false);
+
+    const zoomEnabled = zoom !== "false";
 
     const showImg = img ? "block" : "none";
     const showText = img ? "none" : "block";
 
-    const openModal = () => setIsOpen(true);
+    const openModal = () => {
+        if(zoomEnabled) setIsOpen(true);
+    };
     const closeModal = () => setIsOpen(false);
 
     const content = (
     <div className="bg-gray-900 rounded-xl text-white h-auto overflow-hidden">
         <img
-        src={img || ""}
+        src={img || null}
         alt="Card Image"
         className="h-auto rounded w-full object-cover"
         style={{ display: showImg }}
         />
         <div style={{ display: showText }}>
-        <h1 className="m-4 text-5xl font-bold">{title}</h1>
-        <p className="m-4 text-2xl text-gray-500">{subtitle}</p>
-        <p className="m-4">{text}</p>
+        <h1 className="m-4 xs:m-2 xs:text-3xl sm:text-6xl md:text-5xl xl:text-6xl 2xl:text-7xl font-bold">{title}</h1>
+        <p className="m-4 xs:m-2 xs:text-sm sm:text-3xl md:text-2xl xl:text-3xl 2xl:text-4xl text-gray-500">{subtitle}</p>
+        <p className="m-4 xs:m-2 xs:text-xs sm:text-xl md:text-base xl:text-lg 2xl:text-xl">{text}</p>
         </div>
     </div>
     );
 
     return (
     <>
-        <div onClick={openModal} className="cursor-pointer">
+        <div onClick={zoomEnabled ? openModal : undefined} className={zoomEnabled ? "cursor-pointer" : ""}>
         {content}
         </div>
 
